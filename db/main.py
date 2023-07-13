f76357e0112527c091950e3861479b31ed28f780
chawanghyeon <chawanghyeon@gmail.com>
2023-07-13 23:57:50 +0900

    feature: add type

--- db/main.py
+++ db/main.py
@@ -1,8 +1,34 @@
+from enum import Enum
+
+
 class InputBuffer:
     def __init__(self):
         self.buffer = None
         self.buffer_length = 0
         self.input_length = 0
+
+
+class ExecuteResult(Enum):
+    EXECUTE_SUCCESS = 0
+    EXECUTE_DUPLICATE_KEY = 1
+
+
+class MetaCommandResult(Enum):
+    META_COMMAND_SUCCESS = 0
+    META_COMMAND_UNRECOGNIZED_COMMAND = 1
+
+
+class PrepareResult(Enum):
+    PREPARE_SUCCESS = 0
+    PREPARE_NEGATIVE_ID = 1
+    PREPARE_STRING_TOO_LONG = 2
+    PREPARE_SYNTAX_ERROR = 3
+    PREPARE_UNRECOGNIZED_STATEMENT = 4
+
+
+class StatementType(Enum):
+    STATEMENT_INSERT = 0
+    STATEMENT_SELECT = 1
 
 
 def print_prompt():
